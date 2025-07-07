097eb05f48c3ddb9f4e4e659141f9ac49b977326
Josip Juraj <josip.zuzic@outlook.com>
2025-07-07 11:04:25 +0200

    Dodavanje početnih file-ova za knjigu i knjižnicu
    Prvi zadatak

---
+++ Knjiga.java
@@ -0,0 +1,57 @@
+public class Knjiga {
+
+    private static long idTotal;
+
+    private long id;
+    private String title;
+    private String author;
+    private String genre;
+    private boolean isAvailable;
+
+    public Knjiga(String title, String author, String genre){
+
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+
+        isAvailable = false;
+        id = idTotal;
+        idTotal++;
+    }
+
+    public void ChangeAvailability(){
+        this.isAvailable = !isAvailable;
+    }
+
+    public void ChangeAvailability(boolean availability){
+        this.isAvailable = availability;
+    }
+
+    public String GetTitle(){
+        return title;
+    }
+
+    public  String GetAuthor(){
+        return author;
+    }
+
+    public String GetGenre(){
+        return genre;
+    }
+
+    public long GetId(){
+        return id;
+    }
+
+    public void SetTitle(String title){
+        this.title = title;
+    }
+
+    public void SetAuthor(String author) {
+        this.author = author;
+    }
+
+    public void SetGenre(String genre) {
+        this.genre = genre;
+    }
+}
---
+++ MainKlasa.java
@@ -0,0 +1,103 @@
+import java.util.List;
+
+public class MainKlasa {
+
+    List<Knjiga> libraryBooks;
+
+    public static void main(String[] args) {
+
+    }
+
+    public void AddBook(Knjiga book){
+        libraryBooks.add(book);
+    }
+
+    public void AddBook(String title, String author, String genre){
+        libraryBooks.add(new Knjiga(title, author, genre));
+    }
+
+    public List<Knjiga> SearchBookByTitle(String searchInput){
+
+        List<Knjiga> matchingBooks = null;
+
+        for(Knjiga book : libraryBooks){
+
+            if(book.GetTitle().toLowerCase().contains(searchInput.toLowerCase())){
+                matchingBooks.add(book);
+            }
+        }
+
+        return matchingBooks;
+    }
+
+    public List<Knjiga> SearchBookByAuthor(String searchInput){
+
+        List<Knjiga> matchingBooks = null;
+
+        for(Knjiga book : libraryBooks){
+
+            if(book.GetAuthor().toLowerCase().contains(searchInput.toLowerCase())){
+                matchingBooks.add(book);
+            }
+        }
+
+
+        return matchingBooks;
+    }
+
+    public List<Knjiga> SearchBookByGenre(String searchInput){
+
+        List<Knjiga> matchingBooks = null;
+
+        for(Knjiga book : libraryBooks){
+
+            if(book.GetGenre().toLowerCase().contains(searchInput.toLowerCase())){
+                matchingBooks.add(book);
+            }
+        }
+
+        return matchingBooks;
+    }
+
+    public Knjiga ChangeBookTitle(Knjiga book, String title){
+        if(book == null){
+            System.out.println("Error, book cannot be null!");
+            return null;
+        }
+        book.SetTitle(title);
+        return book;
+    }
+
+    public Knjiga ChangeBookAuthor(Knjiga book, String author){
+
+        if(book == null){
+            System.out.println("Error, book cannot be null!");
+            return null;
+        }
+
+        book.SetAuthor(author);
+        return book;
+    }
+
+    public Knjiga ChangeBookGenre(Knjiga book, String genre){
+
+        if(book == null){
+            System.out.println("Error, book cannot be null!");
+            return null;
+        }
+
+        book.SetGenre(genre);
+        return book;
+    }
+
+    public void RemoveBookById(long id){
+        for(Knjiga book : libraryBooks){
+            if(book.GetId() == id ){
+                libraryBooks.remove(book);
+                return;
+            }
+        }
+        System.out.println("Book with id of \"" + id + "\" not found!");
+    }
+
+}
